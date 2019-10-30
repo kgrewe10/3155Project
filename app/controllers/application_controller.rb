9ad488044f70f27e8b4e9f2e0bf48a9586f9bb83
Kyle Grewe <kylegrewe10@gmail.com>
2019-10-30 23:30:37 +0000

    Separated home page from all games page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+    def index
+        @games = Game.all
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get "games/topgames.html", to: "games#topgames", as: "topgames"
+  get "home/index"
   resources :games
   
-  root 'games#index'
+  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
